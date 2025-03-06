ea7dc658346503381d8a6c7e61e300e46a299d24
rachit4018 <rachitpandya2509@gmail.com>
2025-03-06 00:03:57 -0500

    SCRUM-8 deleted jest.config.js

--- frontend/ex_tracker/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    testMatch: [
-      "**/test/**/*.js",       // Look for tests in the `test` directory
-      "**/?(*.)+(spec|test).js" // Look for files with .spec.js or .test.js
-    ],
-    testPathIgnorePatterns: [
-      "/node_modules/"  // Ignore node_modules
-    ],
-  };
